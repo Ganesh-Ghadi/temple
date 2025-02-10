82c05440ba5f45c213a0a322418c449eb8cb9b73
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2025-02-10 14:00:14 +0530

    today saree and uparane added in gotravali report

--- backend/resources/views/Reports/GotravaliReport/index.blade.php
+++ backend/resources/views/Reports/GotravaliReport/index.blade.php
@@ -48,6 +48,23 @@
      <h4 style=" margin:10px 0px 15px; 0px;">एकुण {{$poojaType}}: {{$totalNamesForPoojaType}} </h4>
 @endforeach
 
+<div style="margin:0px; padding:0px; border-bottom: 1px solid gray;">
+   
+</div>
+
+<div style="margin:0px; padding:0px; border-bottom: 1px solid gray;">
+    <h4>साडी</h4>
+    <div style="margin:0px; padding:0px">
+            <p>{{$sareeReceipt->name}}</p> 
+    </div>  
+</div>
+
+<div style="margin:0px; padding:0px; border-bottom: 1px solid gray;">
+    <h4>उपरणे</h4>
+    <div style="margin:0px; padding:0px">
+            <p>{{$uparaneReceipt->name}}</p> 
+    </div>  
+</div>
     </body>
 
 </html>
