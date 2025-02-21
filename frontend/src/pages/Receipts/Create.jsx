aa61eaa441b9ad56158f884c1c8ddcc0fe183eca
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2025-02-21 07:37:56 +0530

    selectAll cehckbox fixed

--- frontend/src/pages/Receipts/Create.jsx
+++ frontend/src/pages/Receipts/Create.jsx
@@ -501,6 +501,22 @@ const Create = () => {
     }
   };
 
+  const handleSelectAllChange = () => {
+    if (selectAllCheckbox) {
+      // If Select All is already checked, uncheck all checkboxes and set empty array
+      setSelectedDates([]);
+      updateAmount(0);
+    } else {
+      // If Select All is unchecked, check all checkboxes
+      const allDates = poojaDatesData?.PoojaDates.map(
+        (poojaDate) => poojaDate.pooja_date
+      );
+      setSelectedDates(allDates);
+      updateAmount(allDates.length);
+    }
+    setSelectAllCheckbox(!selectAllCheckbox);
+  };
+
   // Update amount for date checkboxes
   const updateAmount = (numOfDates) => {
     numOfDates = parseFloat(numOfDates);
@@ -2974,20 +2990,7 @@ const Create = () => {
                               //   setSelectAllCheckbox(!selectAllCheckbox);
                               // }}
                               checked={selectAllCheckbox} // Bind the state to the checkbox
-                              onChange={() => {
-                                setSelectAllCheckbox(!selectAllCheckbox); // Toggle the state
-                                // Select/deselect all checkboxes
-                                poojaDatesData?.PoojaDates?.forEach(
-                                  (poojaDate) => {
-                                    handleCheckboxChange(
-                                      {
-                                        target: { checked: !selectAllCheckbox },
-                                      },
-                                      poojaDate.pooja_date
-                                    );
-                                  }
-                                );
-                              }}
+                              onChange={handleSelectAllChange} // Use the updated function
                               className="peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
                             />
                           )}
